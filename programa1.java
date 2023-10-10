20fc2d984561759c0a3dbadaf42574f1ef72aeca
Angel <asegui20218@iesjoanramis.org>
2023-10-10 18:06:35 +0200

    Canvis dins rama comentaris

--- programa1.java
+++ programa1.java
@@ -2,3 +2,11 @@
 //Comentarios desde la rama modificacion
 //Otro comentario desde rama master
 //Otra modificacion de programa1
+
+//Comentari dins de la rama comentaris
+
+/*
+Hola
+Comentaris
+*/
+
