5814b33745ca1873a0855de6e9eadd9419cfeaa0
Angel <asegui20218@iesjoanramis.org>
2023-10-17 17:32:21 +0200

    Revert "Canvi master programa suma"
    This reverts commit 567442718ed8b9a1fbbfafcd6e4945a7c377a52f.

--- programa1.java
+++ programa1.java
@@ -23,21 +23,3 @@ public class ProducteNombres {
     }
 }
 
-import java.util.Scanner;
-
-public class SumaDosNumeros {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Posa un numero: ");
-        double numero1 = scanner.nextDouble();
-
-        System.out.print("Posa un altre numero: ");
-        double numero2 = scanner.nextDouble();
-
-        double suma = numero1 + numero2;
-
-        System.out.println("La suma es: " + suma);
-    }
-}
-
